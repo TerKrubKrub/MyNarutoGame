7b6b539c4630ed4110c814f2b97cd8ab6227d39d
Jirakan Koollasing <TerKrub084@gmail.com>
2020-12-08 04:30:22 +0700

    .

--- MyGame/Hp.cpp
+++
@@ -1,1 +0,0 @@
-#include "Hp.h"
--- MyGame/shoot.cpp
+++ MyGame/shoot.cpp
@@ -2,9 +2,6 @@
 
 shoot::shoot()
 {
-	/*rect.setSize(sf::Vector2f(25, 25));
-	rect.setPosition(0, 0);
-	rect.setFillColor(sf::Color::Green);*/
 	sprite.setTextureRect(sf::IntRect(0, 0, 373, 373));
 	sprite.setScale(1/20.f, 1/20.f);
 }
